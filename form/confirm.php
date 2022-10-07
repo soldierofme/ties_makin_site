8800598f37e134cfc50b00536ba7922cfc9a82e4
soldierofme <49803975+soldierofme@users.noreply.github.com>
2022-10-07 09:18:36 +0900

    コミット

---
+++ form/confirm.php
@@ -0,0 +1,52 @@
+<!-- ロジック
+================================================================================================ -->
+<?php
+// セッション開始
+session_start();
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    //フォームのボタンが押されたら、POSTされたデータを各変数に格納
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $message = $_POST["message"];
+
+    // トークンの生成（CSRF対策）
+    $token = bin2hex(random_bytes(32));
+    $_SESSION['token'] = $token;
+
+    // HTML出力前のエスケープ処理
+    function escape($str)
+    {
+        return htmlspecialchars($str, ENT_QUOTES, 'UTF-8');
+    }
+} else {
+    //フォームボタン以外からこのページにアクセスした場合（URL直接入力など）、トップページに戻る
+    header(("location: alert.php"));
+    exit;
+}
+?>
+
+<!-- ビュー
+================================================================================================ -->
+<h2>Confirm</h2>
+<p class="confirm-text">下記の内容でメッセージを送信します。よろしければ「送信」ボタンを押してください。</p>
+<form action="complete.php" method="post">
+    <input type="hidden" name="token" value="<?php echo escape($token); ?>">
+    <div class="contact-form">
+        <label for="name">お名前</label>
+        <input type="hidden" id="name" name="name" value="<?php echo escape($name); ?>">
+        <p><?php echo escape($name); ?></p>
+    </div>
+    <div class="contact-form">
+        <label for="email">メールアドレス</label>
+        <input type="hidden" id="email" name="email" value="<?php echo escape($email); ?>">
+        <p><?php echo escape($email); ?></p>
+    </div>
+    <div class="contact-form">
+        <label for="message">メッセージ</label>
+        <input type="hidden" id="message" name="message" value="<?php echo escape($message); ?>">
+        <p><?php echo nl2br(escape($message)); ?></p>
+    </div>
+    <input class="btn" type="button" value="修正" onclick="history.back(-1)">
+    <input class="btn" type="submit" value="送信" name="submit"></input>
+</form>
